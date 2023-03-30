446eb287dfbdc28ce01d2e975f3e349df87e65ec
Brett Abramson <brett.m.abramson@gmail.com>
2023-03-30 10:55:18 -0500

    hiking app.js routes added and low-key tested

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import axios from "axios"
 import Camp from "./components/camping/camp"
 
 const App = () => {
+  //  === CAMPING ===
   const [camps, setCamps] = useState([])
   
   
   const getCamps = () => {
-    // axios.get(PORT + "/camping")
-    axios.get("http://localhost:3000/camping").then((response) => {
-      setCamps(response.data)
-      })
+    axios.get("http://localhost:3000/camping")
+    .then((response) => setCamps(response.data), (err) => console.oog(err))
+    .catch((error) => console.log(error))
     }
   
   const handleCreateCamp = (data) => {
@@ -23,6 +23,44 @@ const App = () => {
     })
   }
   
+
+  /// === HIKING  ===
+  const [hikes, setHikes] = useState([])
+
+  const getHikes = () => {
+    // axios.get(PORT + "/camping")
+    axios.get("http://localhost:3000/hiking")
+    .then((response) => {
+      setCamps(response.data)
+      })
+    }
+  
+  const handleCreateHike = (data) => {
+    axios.post("http://localhost:3000/hiking", data).then((response) => {
+      console.log(response);
+      let newHikes = [...hikes, response.data]
+      setCamps(newHikes)
+    })
+  }
+  const handleDeleteHike = (deletedHike) => {
+    axios.delete("http://localhost:3000/hiking/" + deletedHike._id)
+    .then((response) => {
+      let newHikes = hikes.filter((hike) => {
+        return hike._id !== deletedHike._id
+      })
+      setHikes(newHikes)
+    })
+  }
+  const handleEditHike = (data) => {
+    axios.put("http://localhost:3000/hiking/" + data._id, data)
+    .then((response) => {
+      console.log(response)
+      let newHike = hikes.map((hike) => {
+        return hike._id !== data._id ? hike : data
+      })
+      setHikes(newHike)
+    })
+  }
   
 
   useEffect(() => {
