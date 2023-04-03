d0c6278ae8a980ab709d63cfea3e86390a387d01
BethCries <b.jeanne.mills@gmail.com>
2023-04-03 13:18:06 -0400

    added add hike and camp feature on index pages. worked on responsive design for nav bars

--- src/components/camping/camp.js
+++ src/components/camping/camp.js
@@ -1,8 +1,29 @@
 import "../../style/index.css"
 import "../../style/home.css"
 
+import AddCamp from "../camping/add"
+
+import {useState} from "react"
+import axios from "axios"
 
 const Camp = (props) => {
+    const [camps, setCamps] = useState([])
+    const [showAddCamp, setshowAddCamp] = useState(false);
+
+    const handleCreateCamp = (data) => {
+        axios.post("http://localhost:3000/camping", data).then((response) => {
+          console.log(response);
+          let newCamps = [...camps, response.data]
+          setCamps(newCamps)
+        })
+      }
+
+      const toggleAddCamp = () => {
+        setshowAddCamp(!showAddCamp);
+      };
+
+
+
     return(
         <>
         {/* <div className="page-container"> */}
@@ -12,19 +33,30 @@ const Camp = (props) => {
                 <nav className="navbar-camp">
                 <div className="nav-bar-photo-and-text" id="hiker-background">
                 <img src="https://i.imgur.com/dwHSPgj.png" alt="hiking man" id="hiker"/>
-                <p id="hiking-man-text">Hiking</p>
+                <p id="hiking-man-text">Camping</p>
                 </div>
                 <div className="nav-bar-photo-and-text" >
+                <a onClick={toggleAddCamp}>
                 <img src="https://i.imgur.com/jRAPlMA.png" alt="add" id="add-hike"/>
-                <p>Add hike</p>
+                <p>Add Site</p>
+                {showAddCamp && (
+                              <div className="modal">
+                                <div onClick={toggleAddCamp} className="overlay"></div>
+                                <div className="modal-content">
+                              <AddCamp handleCreate={handleCreateCamp}/>
+                              <button className="close-modal" onClick={toggleAddCamp}>X</button>
+                              </div>
+                              </div>
+                              )}
+                </a>
                 </div>
                 <div className="nav-bar-photo-and-text" >
-                <img src="https://i.imgur.com/NNaDgju.png" alt="parking filter" id="parking"/>
-                <p>Parking</p>
+                <img src="https://i.imgur.com/Ctj1SAH.png" alt="parking filter" id="electric"/>
+                <p>Electric</p>
                 </div >
                 <div className="nav-bar-photo-and-text" >
-                <img src="https://i.imgur.com/g9mXD0D.png" alt="kid friendly" id="kids"/>
-                <p>Kid Friendly</p>
+                <img src="https://i.imgur.com/owyxVAj.png" alt="kid friendly" id="laundry"/>
+                <p>Laundry</p>
                 </div>
                 <div className="nav-bar-photo-and-text">
                 <img src="https://i.imgur.com/Ydl3t1q.png"  id="pets" alt="pet friendly"/>
